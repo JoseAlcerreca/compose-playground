3ffe67f006702d8c2a24220d8c83559aacef822d
Jose Alcérreca <jalc@google.com>
2021-01-27 11:49:21 +0000

    reenables countertest with is failing in alpha11

--- app/src/androidTest/java/com/example/jalc/myapplication/CounterTest.kt
+++ app/src/androidTest/java/com/example/jalc/myapplication/CounterTest.kt
@@ -1,34 +1,32 @@
-//package com.example.jalc.myapplication
-//
-//import androidx.compose.ui.test.*
-//import androidx.compose.ui.test.junit4.createAndroidComposeRule
-//import androidx.compose.ui.test.junit4.createAndroidComposeRuleLegacy
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//
-//import org.junit.Rule
-//
-//
-//@RunWith(AndroidJUnit4::class)
-//class CounterTest {
-//
-//    @get:Rule
-//    val composeTestRule = createAndroidComposeRule<MainActivity>()
-//
-//    @Test
-//    fun counter_initially_zero() {
-//        // Given a counter in its initial state, assert that it has zero clicks
-//        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.clicks_count, 0)).assertExists()
-//    }
-//
-//    @Test
-//    fun clickButton_incrementsCounter() {
-//        // Click on the button to increment the number of clicks
-//        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.bt_increment_counter)).performClick()
-//
-//        // Check that the counter was incremented
-//        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.clicks_count, 1)).assertExists()
-//    }
-//}
+package com.example.jalc.myapplication
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class CounterTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun counter_initially_zero() {
+        // Given a counter in its initial state, assert that it has zero clicks
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.clicks_count, 0)).assertExists()
+    }
+
+    @Test
+    fun clickButton_incrementsCounter() {
+        // Click on the button to increment the number of clicks
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.bt_increment_counter)).performClick()
+
+        // Check that the counter was incremented
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.clicks_count, 1)).assertExists()
+    }
+}
